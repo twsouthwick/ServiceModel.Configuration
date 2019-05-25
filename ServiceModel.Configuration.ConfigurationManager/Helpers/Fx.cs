543e3719f030e5d04dd50512a369bc01eede81ed
Taylor Southwick <tasou@microsoft.com>
2019-05-24 19:48:42 -0700

    Add Fx.cs

---
+++ ServiceModel.Configuration.ConfigurationManager/Helpers/Fx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    internal static class Fx
+    {
+        internal static class Tag
+        {
+            internal sealed class SecurityNoteAttribute : Attribute
+            {
+                public string Critical { get; set; }
+
+                public string Safe { get; set; }
+            }
+        }
+    }
+}
